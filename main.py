2d5d874344d7c852c8e37c69594a8e84828be3da
Ceramik9 <grad.sebastian@gmail.com>
2026-01-11 14:30:14 +0000

    finished bookbot

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+#frankenstein_path = "books/frankenstein.txt"
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+# word_counter - accepts file path and returns total word count
+from stats import word_counter
+total_word_count = word_counter(sys.argv[1])
+
+# character_counter - accepts file path and returns total character count
+from stats import character_counter
+total_char_count = character_counter(sys.argv[1])
+
+# refactor character counter to correct format
+from stats import refactor_dict
+new_character_count = refactor_dict(total_char_count)
+
+# sort characters starting with highest number
+from stats import sort_on
+new_character_count.sort(reverse=True, key=sort_on)
+
+def main():
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print("----------- Word Count ----------")
+    print(total_word_count)
+    print("--------- Character Count -------")
+    for character in new_character_count:
+        print(f"{character["character"]}: {character["number"]}")
+    print("============= END ===============")
+
+main()
+
---
+++ stats.py
@@ -0,0 +1,28 @@
+def word_counter(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    words = file_contents.split()
+    return f"Found {len(words)} total words"
+
+def character_counter(file_path):
+    list_of_letters = list()
+    dict_letters = dict()
+    with open(file_path) as f:
+        file_contents = f.read()
+    for letter in file_contents:
+        list_of_letters.append(letter.lower())
+    for char in list_of_letters:
+        if char not in dict_letters:
+            dict_letters[char] = 1
+        else:
+            dict_letters[char] += 1
+    return dict_letters
+
+def refactor_dict(my_dict):
+    new_dict = []
+    for key in my_dict:
+        new_dict.append({"character": key, "number": my_dict[key]})
+    return new_dict
+
+def sort_on(item):
+    return item["number"]
